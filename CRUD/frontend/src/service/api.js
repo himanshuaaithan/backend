19ce7ed53ba79e17e30de8df7c4f9dc4d1fb1ea8
himanshu aaithan <himanshukumawat9649@gmail.com>
2026-06-19 12:18:15 +0530

    api change

--- CRUD/frontend/src/service/api.js
+++ CRUD/frontend/src/service/api.js
@@ -1,25 +1,25 @@
 import axios from 'axios'
 
 export const NotesDataFatched = async () => {
-    const res = await axios.get('http://localhost:3000/api/notes')
+    const res = await axios.get('https://backend-ryor.onrender.com/api/notes')
     return res.data
 }
 
 
 export const NotesCreateApi = async (title, description) => {
-    await axios.post('http://localhost:3000/api/notes', {
+    await axios.post('https://backend-ryor.onrender.com/api/notes', {
         title, description
     })
 }
 
 export const NotesDeleteApi = async (id) => {
 
-    await axios.delete(`http://localhost:3000/api/notes/${id}`)
+    await axios.delete(`https://backend-ryor.onrender.com/api/notes/${id}`)
 
 }
 
 export const noteUpdateApi = async (id, description, title) => {
-    const res = await axios.put(`http://localhost:3000/api/notes/${id}`, {
+    const res = await axios.put(`https://backend-ryor.onrender.com/api/notes/${id}`, {
         title,
         description
     })
